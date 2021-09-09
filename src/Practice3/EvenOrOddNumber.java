ec7d20e1b23a60647a8a423aee632eb9724f7a0b
semanuels <semanuels@inbox.lv>
2021-09-09 16:43:45 +0100

    created even or odd numbers and if else condition

---
+++ src/Practice3/EvenOrOddNumber.java
@@ -0,0 +1,16 @@
+package Practice3;
+
+public class EvenOrOddNumber {
+
+    public static void main(String[] args){
+
+        int num = 44;
+
+        if (num%2 == 0){
+            System.out.println("Number is even");
+        }
+        else {
+            System.out.println("Number is odd");
+        }
+    }
+}
---
+++ src/Practice3/IfElseCondition.java
@@ -0,0 +1,16 @@
+package Practice3;
+
+public class IfElseCondition {
+
+    public static void main(String[] args){
+
+        int age = 37;
+
+        if (age >= 18) {
+            System.out.println("You are old enough to drive!");
+        }
+            else {
+            System.out.println("You are too young to drive.");
+        }
+    }
+}
