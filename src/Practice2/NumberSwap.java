a272c15c83317f9f0f91a3900adb75d0b3a1a619
semanuels <semanuels@inbox.lv>
2021-09-09 15:44:36 +0100

    Created number svap code

---
+++ src/Practice2/NumberSwap.java
@@ -0,0 +1,17 @@
+package Practice2;
+
+public class NumberSwap {
+
+    public static void main (String[] args){
+        int a = 12;
+        int b = 5;
+        int c;
+
+        c=b;
+        b=a;
+        a=c;
+
+        System.out.println("A is equal to: " +a);
+        System.out.println("B is equal to: " +b);
+    }
+}
